376aaac749cc96c0432b88222823932951cf880f
juliane <jcasier@protonmail.com>
2022-06-29 20:27:35 +0200

    jetons on home

--- frontend/src/pages/Home/style.jsx
+++ frontend/src/pages/Home/style.jsx
@@ -10,13 +10,22 @@ export default styled.div`
     height: 50vh;
     padding: 1rem;
   }
-
+  .list {
+    display: flex;
+  }
   .button {
-    background-color: var(--color-blue);
-    text-transform: uppercase;
-    color: rgb(240, 240, 240);
+    background-color: var(--color-orange);
     padding: 0.5em 1.75em 0.5em;
     margin: 4vh 0;
     text-align: center;
+  }
+  .jeton {
+    background-color: var(--color-blue);
+    border: 1px solid red;
+    margin: 1rem;
+    border-radius: 50%;
+    padding: 0.9rem;
+    color: white;
+    width: 4rem;
   }
 `;
