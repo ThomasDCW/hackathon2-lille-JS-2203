1cf2e5ce3f8801938c8807ab56c182e5c7f7764b
camille <camille.arvieu@gmail.com>
2022-06-29 09:21:48 +0200

    initializing frontend packages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Reset } from "styled-reset";
 
 import App from "./App";
 
@@ -7,6 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Reset />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
