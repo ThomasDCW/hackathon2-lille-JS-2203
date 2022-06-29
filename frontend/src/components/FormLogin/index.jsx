8bd750e260c978f6d505572243a650ed803b2d38
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-29 16:02:54 +0200

    login page done

--- frontend/src/components/FormLogin/index.jsx
+++ frontend/src/components/FormLogin/index.jsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
 import SFormLogin from "./style";
 
 export default function FormLogin() {
-  return <SFormLogin>LOGIN</SFormLogin>;
+  return (
+    <SFormLogin>
+      <h1>Machine à Projet</h1>
+      <form>
+        <h2>Se connecter</h2>
+        <input type="text" placeholder="Identifiant" />
+        <input type="password" placeholder="Mot de passe" />
+        <div className="textContainer">
+          <p>Se souvenir de moi</p>
+          <p>Mot de passe oublié ?</p>
+        </div>
+      </form>
+      <div>
+        <Link to="/">Connexion</Link>
+      </div>
+      <div className="textContainer">
+        <p>Pas encore de compte ?</p>
+        <p>
+          <span>Créer un compte</span>
+        </p>
+      </div>
+    </SFormLogin>
+  );
 }
--- frontend/src/components/FormLogin/style.jsx
+++ frontend/src/components/FormLogin/style.jsx
@@ -1,5 +1,68 @@
 import styled from "styled-components";
 
-export default styled.form`
-  background-color: red;
+export default styled.div`
+  width: 40vw;
+  height: 80vh;
+  color: #183650;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  h1 {
+    font-size: 2em;
+    margin-top: 1em;
+    margin-bottom: 1em;
+    text-align: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border: 3px solid #e89759;
+    border-radius: 8px;
+    height: 60%;
+    width: 100%;
+    .textContainer {
+      display: flex;
+      justify-content: space-between;
+      font-size: 0.8em;
+      margin: 0.8em;
+    }
+    h2 {
+      margin: 1em;
+      font-size: 1.2em;
+    }
+    input {
+      margin: 1.2em;
+      outline: 0;
+      border-width: 0 0 2px;
+      border-color: #183650;
+      ::placeholder {
+        color: #183650;
+        font-size: 1em;
+      }
+    }
+  }
+  .textContainer {
+    display: flex;
+    justify-content: center;
+    font-size: 0.6em;
+    margin: 0.3em;
+    span {
+      color: #e89759;
+    }
+  }
+  div {
+    margin-top: 2em;
+    display: flex;
+    justify-content: center;
+    a {
+      width: 75%;
+      text-decoration: none;
+      text-align: center;
+      padding: 1em;
+      color: white;
+      background: #183650;
+      border-radius: 8px;
+    }
+  }
 `;
