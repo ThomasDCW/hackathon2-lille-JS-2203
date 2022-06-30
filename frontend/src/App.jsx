135f97f7399a03390ba4fc2749cd049c633c462c
camille <camille.arvieu@gmail.com>
2022-06-30 16:28:59 +0200

    adding new projects fonctionnality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "@pages/Home";
 import Profil from "@pages/Profil";
 import { Routes, Route } from "react-router-dom";
 import ProjectPage from "@pages/ProjectPage";
+import NewProject from "@pages/NewProject";
 import SGlobal from "./style";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/accueil" element={<Home />} />
         <Route path="/perso" element={<Profil />} />
         <Route path="/projets" element={<ProjectPage />} />
+        <Route path="/newprojets" element={<NewProject />} />
       </Routes>
     </SGlobal>
   );
