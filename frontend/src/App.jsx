8e0bf9b6c1e37fa88531cf31bc4a280a7f74d729
juliane <jcasier@protonmail.com>
2022-06-30 14:04:50 +0200

    linking to project detail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "@pages/Home";
 import Profil from "@pages/Profil";
 import { Routes, Route } from "react-router-dom";
 import ProjectPage from "@pages/ProjectPage";
+import ProjectDetailPage from "@pages/ProjectDetailPage";
 import SGlobal from "./style";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/accueil" element={<Home />} />
         <Route path="/perso" element={<Profil />} />
         <Route path="/projets" element={<ProjectPage />} />
+        <Route path="/projets/:id" element={<ProjectDetailPage />} />
       </Routes>
     </SGlobal>
   );
---
+++ frontend/src/pages/ProjectDetailPage/index.jsx
@@ -0,0 +1,12 @@
+import Header from "@components/Header";
+import ProjectDetail from "@components/ProjectDetail";
+import SProjectDetailPage from "./style";
+
+export default function ProjectDetailPage() {
+  return (
+    <SProjectDetailPage>
+      <Header />
+      <ProjectDetail />
+    </SProjectDetailPage>
+  );
+}
