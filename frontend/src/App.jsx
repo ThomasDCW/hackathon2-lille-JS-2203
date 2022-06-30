315e59f851c5c8bb233d8fd03b9c1ba59cbc2905
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-30 14:53:11 +0200

    addsearchuser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "@pages/Home";
 import Profil from "@pages/Profil";
 import { Routes, Route } from "react-router-dom";
 import ProjectPage from "@pages/ProjectPage";
+import SearchUser from "@pages/SearchUser";
 import SGlobal from "./style";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/accueil" element={<Home />} />
         <Route path="/perso" element={<Profil />} />
         <Route path="/projets" element={<ProjectPage />} />
+        <Route path="/utilisateurs" element={<SearchUser />} />
       </Routes>
     </SGlobal>
   );
