25c0f23ad8094b776d68bbe9a8f8bdce397c1d65
Jeremy <jeremy.jorand@gmail.com>
2022-06-30 14:49:01 +0200

    fix css home

--- backend/dump.sql
+++ backend/dump.sql
@@ -171,7 +171,7 @@ DROP TABLE IF EXISTS `projectLife`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `projectLife` (
   `id` int NOT NULL,
-  `step` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -304,4 +304,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-30  1:53:41
+-- Dump completed on 2022-06-30 13:47:05
