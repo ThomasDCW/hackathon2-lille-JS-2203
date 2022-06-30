6aaab01640d30b8174c6881671e22acaa9cdb97f
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-30 12:22:55 +0200

    fix

--- frontend/src/pages/ProjectPage/index.jsx
+++ frontend/src/pages/ProjectPage/index.jsx
@@ -1,12 +1,10 @@
 import Header from "@components/Header";
 import ProjectGallery from "@components/ProjectGallery";
-import Nav from "@components/Nav";
 
 export default function ProjectPage() {
   return (
     <div>
       <Header />
-      <Nav />
       <ProjectGallery />
     </div>
   );
