6cbce2404bf64713ffdefcde8cfc689ea774e11d
romainLF <romainlefloch@hotmail.com>
2022-07-01 10:10:21 +0200

    add link

---
+++ frontend/src/components/Label/style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export default styled.div`
+  height: 23%;
+  width: 31%;
+  background-color: #e9cda0;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  margin: 0.5%;
+  font-size: 0.8em;
+  border: solid 0.1em #18365088;
+  cursor: pointer;
+  @media screen and (max-width: 700px) {
+    font-size: 0.5rem;
+  }
+`;
